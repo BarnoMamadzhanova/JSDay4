6f87e1d35ae635196d84c0baa9733cbb41c53aff
BarnoMamadzhanova <b@mamadzhanova.com>
2023-10-03 15:01:58 +0200

    adding script

--- html/js/intermediate2.js
+++ html/js/intermediate2.js
@@ -1,2 +1,14 @@
 
 
+const click = document.getElementById('btn')
+const colorChange = document.getElementById('bgcolor')
+
+function clickBtn () {
+    let x = Math.floor(Math.random()*256);
+    let y = Math.floor(Math.random()*256);
+    let z = Math.floor(Math.random()*256);
+    const bgColor = `rgb(${x}, ${y}, ${z})`;
+    colorChange.style.backgroundColor = bgColor;
+}
+
+click.addEventListener('click', clickBtn)
