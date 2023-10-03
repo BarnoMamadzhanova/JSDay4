b1b24025cabafbf232372d119cd91b479c51cba7
BarnoMamadzhanova <b@mamadzhanova.com>
2023-10-03 14:23:33 +0200

    intermediate 1 finish

--- intermediate.js
+++ intermediate.js
@@ -1,2 +1,14 @@
 
 
+const santaCl = document.getElementsByClassName('santa');
+
+for (let i = 0; i < santaCl.length; i++) {
+    santaCl[i].addEventListener('click', removeItem)
+}
+
+function removeItem() {
+    this.style.opacity = '0';
+    this.style.transition = 'all 1s';
+    setTimeout(() => {this.remove(); }, 1000)
+}
+ 
